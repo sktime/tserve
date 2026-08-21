d35d0a3713225ce7ab6616fb0027c44992b68a9b
geetu040 <raoarmaghanshakir040@gmail.com>
2026-08-21 08:14:07 +0200

    udpate client

--- src/fomo/client/client.py
+++ src/fomo/client/client.py
@@ -4,7 +4,7 @@ from typing import Any
 
 from fomo.client.transports.http import HttpTransport
 from fomo.types import ForecastRequest, ForecastResponse, HealthResult, ModelsResult
-from fomo.types.converters import frame_to_format, table_format, table_to_frame
+from fomo.types.converters import encode_request, decode_response
 
 
 class Client:
@@ -28,11 +28,6 @@ class Client:
         quantiles: list[float] | None = None,
         params: dict[str, Any] | None = None,
     ) -> ForecastResponse:
-        fmt = table_format(history)
-        history = table_to_frame(history)
-        future = table_to_frame(future) if future is not None else None
-        static = table_to_frame(static) if static is not None else None
-
         request = ForecastRequest(
             history=history,
             time=time,
@@ -48,10 +43,19 @@ class Client:
             quantiles=quantiles,
             params=params,
         )
-        response = self._transport.forecast(request)
-        response.predictions = frame_to_format(response.predictions, fmt)
+
+        # 1. encode request to json + bytes
+        req_metadata, req_bytes_encoded = encode_request(request)
+        # 2. send request to transport
+        res_metadata, res_bytes_encoded = self._transport.forecast(req_metadata, req_bytes_encoded)
+        # 3. decode response to json + bytes
+        response = decode_response(res_metadata, res_bytes_encoded)
+
+        # better to convert response to original format as request had?
+        response.predictions = response.predictions.to_dict()
         if response.quantiles is not None:
-            response.quantiles = frame_to_format(response.quantiles, fmt)
+            response.quantiles = response.quantiles.to_dict()
+
         return response
 
     def health(self) -> HealthResult:
